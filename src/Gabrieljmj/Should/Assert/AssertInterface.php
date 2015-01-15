61841bf7a8f1e73f5c273802906508b6e59db347
Gabriel Jacinto <gamjj74@hotmail.com>
2015-01-15 14:17:01 -0200

    Update license

--- src/Gabrieljmj/Should/Assert/AssertInterface.php
+++ src/Gabrieljmj/Should/Assert/AssertInterface.php
@@ -2,9 +2,10 @@
 /**
  * ShouldPHP
  *
- * @author Gabriel Jacinto <gamjj74@hotmail.com>
- * @status dev
- * @link   https://github.com/GabrielJMJ/ShouldPHP
+ * @author  Gabriel Jacinto <gamjj74@hotmail.com>
+ * @status  dev
+ * @link    https://github.com/GabrielJMJ/ShouldPHP
+ * @license MIT
  */
  
 namespace Gabrieljmj\Should\Assert;
--- tests/Gabrieljmj/Should/Condition/TheClass/BeTest.php
+++ tests/Gabrieljmj/Should/Condition/TheClass/BeTest.php
@@ -2,9 +2,10 @@
 /**
  * ShouldPHP
  *
- * @author Gabriel Jacinto <gamjj74@hotmail.com>
- * @status dev
- * @link   https://github.com/GabrielJMJ/ShouldPHP
+ * @author  Gabriel Jacinto <gamjj74@hotmail.com>
+ * @status  dev
+ * @link    https://github.com/GabrielJMJ/ShouldPHP
+ * @license MIT
  */
  
 namespace Test\Gabrieljmj\Should\Condition\TheClass;
