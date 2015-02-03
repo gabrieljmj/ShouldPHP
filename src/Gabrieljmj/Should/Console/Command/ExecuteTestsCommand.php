8e9e7d8388f455b36a6d21045728bc8c5ecc6c9b
Gabriel Jacinto <gamjj74@hotmail.com>
2015-02-03 15:01:29 -0200

    Fix doc

--- src/Gabrieljmj/Should/Console/Command/ExecuteTestsCommand.php
+++ src/Gabrieljmj/Should/Console/Command/ExecuteTestsCommand.php
@@ -53,7 +53,7 @@ class ExecuteTestsCommand extends Command
     private $eventDispatcher;
 
     /**
-     * @var \Symfony\Component\EventDispatcher\Event
+     * @var \Gabrieljmj\Should\Event\ExecutionEventInterface
      */
     private $event;
 
@@ -66,7 +66,7 @@ class ExecuteTestsCommand extends Command
     }
 
     /**
-     * @param \Symfony\Component\EventDispatcher\Event $event
+     * @param \Gabrieljmj\Should\Event\ExecutionEventInterface $event
      */
     public function setEvent(ExecutionEventInterface $event)
     {
